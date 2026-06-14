d03a74343d2dcea88aba5431284bd01e5d01ebbe
Afzaal Hassan <afzaalhassan162@gmail.com>
2026-06-14 14:07:40 +0500

    fix cors for production

--- server/server.ts
+++ server/server.ts
@@ -11,8 +11,9 @@ import projectRoute from "./routes/projectRoute";
 const app = express();
 
 // 1. Global CORS Setup
+// 1. Global CORS Setup
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
