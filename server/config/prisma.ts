5cde5379ce2ba2adab0e45a80d3894d8320b2757
Afzaal Hassan <afzaalhassan162@gmail.com>
2026-06-14 18:35:51 +0500

    fixing errors

--- server/config/prisma.ts
+++ server/config/prisma.ts
@@ -1,15 +1,10 @@
 import 'dotenv/config';
 import pg from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/index.js';
 
-// ❌ DO NOT IMPORT FROM '@prisma/client'
-// import { PrismaClient } from '@prisma/client'; 
-
-// IMPORT FROM YOUR CUSTOM GENERATED PATH
-// (Adjust the number of dots '../' depending on where your config file is relative to 'generated')
-import { PrismaClient } from '../generated/prisma/client';
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter } as any);
 
 export { prisma };
